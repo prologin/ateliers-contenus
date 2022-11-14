ba201090b2cdc716732e1c340f199e1d6f99a648
Grégoire Lefaure <gregoire.lefaure@prologin.org>
2022-11-14 22:56:07 +0100

    Add skeleton file
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@prologin.org>

--- content/chifoumicrobit/index.md
+++ content/chifoumicrobit/index.md
@@ -4,6 +4,7 @@ authors: ["Grégoire 'greg' Lefaure", "Clément 'swarwerth' Nguyen"]
 subtitle: "Apprenez à coder un chifoumi sur un micro:bit"
 date: 2022
 weight: 5
+code_stub_url: ./resources/given_resources/chifoumicrobit.py
 ---
 
 # Introduction au sujet...
---
+++ content/chifoumicrobit/resources/given_resources/chifoumicrobit.py
@@ -0,0 +1,39 @@
+from microbit import *
+from random import randint
+
+# TODO: Creer la liste des possibilités
+possibilites = 
+
+# Le nombre de possibilités
+# La fonction `len` renvoie la longueur d'une liste
+NB_POSSIBILITES = len(possibilites)
+
+# TODO: Choix aléatoire du microbit
+# entre 0 et `NB_POSSIBILITES + 1` exclu
+choix_microbit =
+
+# Le choix du joueur
+# C'est le choix 0 par défaut
+choix_joueur = 0
+
+#TODO: Ajouter la condition d'arrêt de la boucle avant les `:`
+while :
+    # TODO: Afficher le choix actuel du joueur
+
+    # TODO: Que faire si A est appuyé ?
+
+    # TODO: Que faire si B est appuyé ?
+
+
+
+# TODO: Afficher le choix du joueur
+
+display.scroll("VS")
+
+# TODO: Afficher le choix du microbit
+
+# TODO: Si égalité, afficher "Égalité !"
+
+# TODO: Sinon si tu as gagné, afficher "Tu as gagné !"
+
+# TODO: Sinon, tu as perdu, afficher "Tu as perdu..."
