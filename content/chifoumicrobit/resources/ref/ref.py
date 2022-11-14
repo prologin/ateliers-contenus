605fe9e088d7667c271914fb5cf70fd04dc2706c
Grégoire Lefaure <gregoire.lefaure@prologin.org>
2022-11-14 22:18:24 +0100

    Add ref without bonus
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@prologin.org>

---
+++ content/chifoumicrobit/resources/ref/ref.py
@@ -0,0 +1,61 @@
+from microbit import *
+from random import randint
+
+# La liste des possibilités
+possibilites = [Image.SKULL, Image.PACMAN, Image.GHOST]
+
+# Le nombre de possibilités
+# La fonction `len` renvoie la longueur d'une liste
+NB_POSSIBILITES = len(possibilites)
+
+# Choix aléatoire du microbit
+# entre 0 et `NB_POSSIBILITES + 1` exclu
+choix_microbit = randint(NB_POSSIBILITES + 1)
+
+# Le choix du joueur
+# C'est le choix 0 par défaut
+choix_joueur = 0
+
+# On affiche le choix actuel le temps qu'il n'est pas validé
+# par un appui simultané sur A et B
+while not (button_a.is_pressed() and button_b.is_pressed()):
+    # Affiche le choix actuel du joueur
+    display.show(possibilites[choix_joueur])
+
+    # Si A est appuyé
+    if button_a.is_pressed():
+        # On ajoute 1 au choix du joueur
+        # Le modulo permet de revenir au début de la liste des choix
+        choix_joueur = (choix_joueur - 1) % NB_POSSIBILITES
+
+    # Si B est appuyé
+    if button_b.is_pressed():
+        # On ajoute 1 au choix du joueur
+        # Le modulo permet de revenir au début de la liste des choix
+        choix_joueur = (choix_joueur + 1) % NB_POSSIBILITES
+
+    sleep(100)
+
+# Affiche le choix du joueur
+display.show(possibilites[choix_joueur])
+sleep(500)
+
+display.scroll("VS")
+
+# Affiche le choix du microbit
+display.show(possibilites[choix_microbit])
+sleep(500)
+
+# Égalité si les joueurs ont fait le même choix
+if choix_joueur == choix_microbit:
+    display.show("Égalité !")
+
+# Tu as gagné si notre choix bat celui du microbit
+elif choix_joueur == 0 and choix_microbit == 1 or \
+     choix_joueur == 1 and choix_microbit == 2 or \
+     choix_joueur == 2 and choix_microbit == 0:
+         display.show("Tu as gagné !")
+
+# Tu as perdu sinon
+else:
+    display.show("Tu as perdu...")
