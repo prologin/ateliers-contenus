827150f3933b61afe25132d6a7bfaa64e3cb65cd
Grégoire Lefaure <gregoire.lefaure@prologin.org>
2022-11-19 10:36:06 +0100

    Replace the 'play again' bonus by the 'multiplayer' bonus
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@prologin.org>

---
+++ content/chifoumicrobit/resources/ref/ref_radio.py
@@ -0,0 +1,108 @@
+from microbit import *
+from random import randint
+import radio
+
+# La liste des possibilités
+possibilites = [Image.SKULL, Image.PACMAN, Image.GHOST]
+
+# Le nombre de possibilités
+# La fonction `len` renvoie la longueur d'une liste
+NB_POSSIBILITES = len(possibilites)
+
+multijoueur = False
+while True:
+    display.scroll("Multijoueur (A) ou contre le microbit (B) ?")
+    if button_a.was_pressed():
+        multijoueur = True
+        break
+
+    if button_b.was_pressed():
+        multijoueur = False
+        break
+
+if not multijoueur:
+    # Choix aléatoire du microbit
+    # entre 0 et `NB_POSSIBILITES + 1` exclu
+    choix_adversaire = randint(0, NB_POSSIBILITES - 1)
+
+# Le choix du joueur
+# C'est le choix 0 par défaut
+choix_joueur = 0
+
+# On affiche le choix actuel le temps qu'il n'est pas validé
+# par un appui simultané sur A et B
+while not (button_a.is_pressed() and button_b.is_pressed()):
+    # Affiche le choix actuel du joueur
+    display.show(possibilites[choix_joueur])
+
+    # Si A est appuyé
+    if button_a.was_pressed():
+        # On ajoute 1 au choix du joueur
+        # Le modulo permet de revenir au début de la liste des choix
+        choix_joueur = (choix_joueur - 1) % NB_POSSIBILITES
+
+    # Si B est appuyé
+    if button_b.was_pressed():
+        # On ajoute 1 au choix du joueur
+        # Le modulo permet de revenir au début de la liste des choix
+        choix_joueur = (choix_joueur + 1) % NB_POSSIBILITES
+
+    sleep(100)
+
+
+# Gérer la radio si on est en mode `multijoueur`
+if multijoueur:
+    # Allume la radio
+    radio.on()
+
+    # Configure le channel de la radio (entre [0, 83])
+    radio.config(channel=42)
+
+    display.scroll("Attente de l'autre joueur")
+
+    message_adversaire = None
+    while message_adversaire == None:
+        # Essaye de recevoir un message
+        message_adversaire = radio.receive()
+
+        # Envoie le choix du joueur 1
+        radio.send(str(choix_joueur))
+
+    # Nous avons donc reçu le choix du joueur 2
+    # Il faut le convertir en entier pour l'utiliser ensuite
+    choix_joueur_adverse = int(message_adversaire)
+
+    # Éteindre la radio
+    radio.off()
+
+# Éteint toutes les LEDs
+display.clear()
+sleep(500)
+
+# Affiche le choix du joueur
+display.show(possibilites[choix_joueur])
+sleep(1000)
+
+display.scroll("VS")
+
+# Affiche le choix du microbit
+display.show(possibilites[choix_joueur_adverse])
+sleep(1000)
+
+# Égalité si les joueurs ont fait le même choix
+if choix_joueur == choix_joueur_adverse:
+    display.scroll("Egalite !")
+
+# Tu as gagné si notre choix bat celui du microbit
+elif choix_joueur == 0 and choix_joueur_adverse == 1:
+    display.scroll("Tu as gagne !")
+
+elif choix_joueur == 1 and choix_joueur_adverse == 2:
+    display.scroll("Tu as gagne !")
+
+elif choix_joueur == 2 and choix_joueur_adverse == 0:
+    display.scroll("Tu as gagne !")
+
+# Tu as perdu sinon
+else:
+    display.scroll("Tu as perdu...")
